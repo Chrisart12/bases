1eeafd009c25fc0ca3f41eb6b155b7b8649775d9
Chrisart12 <chrisissifou@gmail.com>
2021-02-28 12:08:39 +0100

    echo

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="style.css" rel="stylesheet">
 </head>
 <body>
     <h1>Apprendre le PHP</h1>
-    
+    <?php echo "je suis un mec sympa"; ?>
+    <div class="<?= 'test';?> <?= 'test2';?>">tototo</div>
 </body>
 </html>
